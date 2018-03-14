9b317c52eec8c4afe3360b61ba9b644f8c3c45c1
rickardinho <d.rickardinho@gmail.com>
2018-03-14 21:56:26 +0000

    hiSetup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/home.js';
+import About from './components/about.js';
+import Download from './components/download.js';
+import Help from './components/help.js';
+import Press from './components/press.js';
+import Contact from './components/contact.js';
+import Terms from './components/terms.js';
+import Privacy from './components/privacy.js';
+import Faq from './components/faq.js';
+import Navbar from './components/general/navbar.js';
+import Footer from './components/general/footer.js';
+import { store } from './init-store.js';
+
+const Routes = () => {
+
+  return (
+
+
+
+    <Router>
+      <div>
+        <Navbar />
+
+        <Route exact path="/" component={ Home } />
+        <Route path='/about' component={ About } />
+        <Route path='/download' component={ Download } />
+        <Route path='/help' component={ Help } />
+        <Route path='/press' component={ Press } />
+        <Route path='/contact' component={ Contact } />
+        <Route path='/terms' component={ Terms } />
+        <Route path='/privacy' component={ Privacy } />
+        <Route path='/faq' component={ Faq } />
+
+        <Footer />
+
+
+      </div>
+    </Router>
+
+  );
+
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Provider store={ store } >
+
+          <Routes />
+
+        </Provider>
       </div>
     );
   }
