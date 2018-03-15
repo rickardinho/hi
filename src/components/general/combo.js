aa1188f844724bc99cde41a215a3f7cd7a8cd3cf
rickardinho <d.rickardinho@gmail.com>
2018-03-15 19:59:17 +0000

    commitCombo

---
+++ src/components/general/combo.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Select from 'react-select';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+import 'react-select/dist/react-select.css';
+
+export default class Combo extends Component {
+
+  render() {
+
+    const options = [
+      { value: 1, label: '1' },
+      { value: 2, label: 2 },
+      { value: 3, label: 3 },
+      { value: 4, label: 4 },
+      { value: 5, label: 5 },
+      { value: 6, label: 6 },
+      { value: 7, label: 7 },
+    ];
+    const defaultOption = options[0];
+
+    return (
+      <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />
+
+    );
+  }
+}
