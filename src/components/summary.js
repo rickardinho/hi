285a824b38ae393cf32daefb30cfe21963fd3658
rickardinho <d.rickardinho@gmail.com>
2018-03-22 12:28:54 +0000

    Update summary.js

--- src/components/summary.js
+++ src/components/summary.js
@@ -1,4 +1,43 @@
- 
+import React from 'react';
+import { render } from 'react-dom';
+import colours from './../../styles/colours';
+import Combo from './combo';
+
+const data = [
+  {
+    date: '1 Jan',
+    rainfall: 5,
+    sunshine: 4
+  },{
+    date: '2 Jan',
+    rainfall: 2,
+    sunshine: 8
+  },{
+    date: '3 Jan',
+    rainfall: 7,
+    sunshine: 2
+  },
+  ,{
+    date: '4 Jan',
+    rainfall: 3,
+    sunshine: 4
+  },
+  ,{
+    date: '5 Jan',
+    rainfall: 2,
+    sunshine: 7
+  },
+  ,{
+    date: '6 Jan',
+    rainfall: 1,
+    sunshine: 6
+  },
+  ,{
+    date: '7 Jan',
+    rainfall: 6,
+    sunshine: 4
+  }
+]
 
     const Feed  = ({ allEvents, notifications, isFetching, handleUpdateNotification, displaySome, displayAll, feedIsFiltered, isShowHosting }) => {
 
