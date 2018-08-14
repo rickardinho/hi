c8dfd67d57753518e8bd93cd7a9c3ab5b3e021b5
rickardinho <d.rickardinho@gmail.com>
2018-08-14 11:14:56 +0100

    Update lineChart.js

--- src/components/lineChart.js
+++ src/components/lineChart.js
@@ -4,11 +4,11 @@ const lineChart = (
   <Chart
     data={[
       {
-        label: "Series 1",
+        label: "SMD",
         data: [[0, 1], [1, 2], [2, 4], [3, 2], [4, 7]]
       },
       {
-        label: "Series 2",
+        label: "Rainfall",
         data: [[0, 3], [1, 1], [2, 5], [3, 6], [4, 4]]
       }
     ]}
