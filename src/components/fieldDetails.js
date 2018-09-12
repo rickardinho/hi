7676247284e194567acf7c0fd016575aa09abeba
rickardinho <d.rickardinho@gmail.com>
2018-09-12 17:46:31 +0100

    Update fieldDetails.js

--- src/components/fieldDetails.js
+++ src/components/fieldDetails.js
@@ -177,17 +177,12 @@ class FieldDetails extends Component {
           style={customStyles}
           contentLabel="Example Modal"
         >
+      <div>
 
-          <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+          <h2>Hello</h2>
           <button onClick={this.closeModal}>close</button>
-          <div>I am a modal</div>
-          <form>
-            <input />
-            <button>tab navigation</button>
-            <button>stays</button>
-            <button>inside</button>
-            <button>the modal</button>
-          </form>
+          
+      </div>
         </Modal>
 
       </MainWrapper>
