cc931eba08c76f1235fc3156e5353cb2e3729b95
rickardinho <d.rickardinho@gmail.com>
2018-09-13 14:50:59 +0100

    Update modal.js

--- src/components/general/modal.js
+++ src/components/general/modal.js
@@ -1,3 +1,15 @@
+
+return (
+  <div>
+    <div className="modal-overlay-div" style={overlayStyle} />
+    <div className="modal-content-div" style={contentStyle} onClick={this.onOverlayClick.bind(this)}>
+      <div className="modal-dialog-div" style={dialogStyle} onClick={this.onDialogClick}>
+        {this.props.children}
+      </div>
+    </div>
+  </div>
+);
+
 import Modal from 'react-modal';
  
 const customStyles = {
