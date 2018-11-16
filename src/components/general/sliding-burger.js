4e26be122e6d65fb669dd9498cac195d83ee66e6
rickardinho <d.rickardinho@gmail.com>
2018-11-16 10:56:13 +0000

    fixBurger

--- src/components/general/sliding-burger.js
+++ src/components/general/sliding-burger.js
@@ -1,55 +1,70 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { NavButton, NavLabel, BurgerDiv } from './../../styles/styles';
+import { Row, H3, H4, LogoWrapper, NavButton, NavLabel, BurgerDiv } from '../../styles/styles';
+import colours from '../../styles/colours';
 
 
-const SlidingBurgerContent = ({onBurgerClick}) => (
+const SlidingBurgerContent = ({ onBurgerClick }) => (
 
-      <BurgerDiv onClick={() => onBurgerClick()}>
-        <div>
-          <NavButton>
+  <BurgerDiv onClick={() => onBurgerClick()}>
+    <div>
+      <NavButton>
+        <LogoWrapper>
 
-          </NavButton>
-        </div>
-
-        <Link to='/' onClick={() => this.closeMenu()}>
-            <NavButton>
-
-                <NavLabel>Farm Summary</NavLabel>
-            </NavButton>
-        </Link>
-
-
-        <Link to='/farmInfo' onClick={() => this.closeMenu()}>
-            <NavButton>
-
-                <NavLabel>Farm Information</NavLabel>
-            </NavButton>
-        </Link>
-
-        <Link to='/fieldDetails' onClick={() => this.closeMenu()}>
-            <NavButton>
-
-                <NavLabel>Field & Crop Details</NavLabel>
-            </NavButton>
-        </Link>
-
-        <Link to='/help' onClick={() => this.closeMenu()}>
-            <NavButton>
-
-                <NavLabel>Help</NavLabel>
-            </NavButton>
-        </Link>
-
-        <Link to='/about' onClick={() => this.closeMenu()}>
-            <NavButton>
-
-                <NavLabel>About</NavLabel>
-            </NavButton>
-        </Link>
+          <Row style={{ background: colours.verylightgray }}>
+            <H3>Happy Irrigator&nbsp;</H3>
+          </Row>
+          <div style={{ borderRadius: 30, background: 'black', borderStyle: 'solid', borderColor: 'black', borderWidth: 1 }}>
+            <i style={{ fontSize: 24, color: 'yellow' }} className='fas fa-grin' />
+          </div>
+          <Row style={{ background: colours.verylightgray }}>
+            <H4>&nbsp;v2.0</H4>
+          </Row>
 
 
-      </BurgerDiv>
+        </LogoWrapper>
+      </NavButton>
+    </div>
+
+
+    <Link to='/' onClick={() => this.closeMenu()}>
+        <NavButton>
+
+            <NavLabel>Farm Summary</NavLabel>
+        </NavButton>
+    </Link>
+
+
+    <Link to='/farmInfo' onClick={() => this.closeMenu()}>
+        <NavButton>
+
+            <NavLabel>Farm Information</NavLabel>
+        </NavButton>
+    </Link>
+
+    <Link to='/fieldDetails' onClick={() => this.closeMenu()}>
+        <NavButton>
+
+            <NavLabel>Field & Crop Details</NavLabel>
+        </NavButton>
+    </Link>
+
+    <Link to='/help' onClick={() => this.closeMenu()}>
+        <NavButton>
+
+            <NavLabel>Help</NavLabel>
+        </NavButton>
+    </Link>
+
+    <Link to='/about' onClick={() => this.closeMenu()}>
+        <NavButton>
+
+            <NavLabel>About</NavLabel>
+        </NavButton>
+    </Link>
+
+
+  </BurgerDiv>
 
 );
 
