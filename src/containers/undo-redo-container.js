acdbaf5ead98b61f80bff3f0bffa52416f225886
rickardinho <d.rickardinho@gmail.com>
2019-06-04 21:12:10 +0100

    Create undo-redo-container.js

---
+++ src/containers/undo-redo-container.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+/* ... */
+
+let UndoRedo = ({ canUndo, canRedo, onUndo, onRedo }) => (
+  <p>
+    <button onClick={onUndo} disabled={!canUndo}>
+      Undo
+    </button>
+    <button onClick={onRedo} disabled={!canRedo}>
+      Redo
+    </button>
+  </p>
+);
+
+/* ... */
+
+import { ActionCreators as UndoActionCreators } from 'redux-undo'
+import { connect } from 'react-redux'
+
+/* ... */
+
+const mapStateToProps = state => {
+  return {
+    canUndo: state.todos.past.length > 0,
+    canRedo: state.todos.future.length > 0
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onUndo: () => dispatch(UndoActionCreators.undo()),
+    onRedo: () => dispatch(UndoActionCreators.redo())
+  }
+}
+
+UndoRedo = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UndoRedo);
+
+export default UndoRedo;
+
